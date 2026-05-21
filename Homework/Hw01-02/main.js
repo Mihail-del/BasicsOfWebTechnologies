a2c96aead9207a1127ac54567e7a78ef29667137
mykhailopulov <mihail.pulov.247@gmail.com>
2026-05-21 23:15:09 +0300

    feat: connect js with product list

---
+++ Homework/Hw01-02/main.js
@@ -0,0 +1,5 @@
+let items = [
+    { id: 1, name: 'Помідори', count: 1, isBought: false },
+    { id: 2, name: 'Печиво', count: 1, isBought: false },
+    { id: 3, name: 'Сир', count: 1, isBought: false }
+];
