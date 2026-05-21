0a74e747e33701c98e40170b0e4043555efd865c
mykhailopulov <mihail.pulov.247@gmail.com>
2026-05-21 23:19:16 +0300

    feat: render dynamic list

--- Homework/Hw01-02/main.js
+++ Homework/Hw01-02/main.js
@@ -3,3 +3,36 @@ let items = [
     { id: 2, name: 'Печиво', count: 1, isBought: false },
     { id: 3, name: 'Сир', count: 1, isBought: false }
 ];
+
+const itemList = document.querySelector('.item-list');
+const inputAdd = document.querySelector('#input-add');
+const btnAdd = document.querySelector('.btn-add');
+
+function render() {
+    itemList.innerHTML = '';
+
+    items.forEach((item) => {
+        const li = document.createElement('li');
+        li.className = 'item-row';
+
+        li.innerHTML = `
+            <span class="item-name">${item.name}</span>
+            <div class="item-controls">
+                <button class="btn-minus" data-tooltip="Зменшити кількість">-</button>
+                <span class="item-count">${item.count}</span>
+                <button class="btn-plus" data-tooltip="Збільшити кількість">+</button>
+            </div>
+            <div class="item-actions">
+                <button class="btn-buy" data-tooltip="Відмітити як куплене">Куплено</button>
+                <button class="btn-delete" data-tooltip="Видалити товар">✖</button>
+            </div>
+        `;
+
+        itemList.appendChild(li);
+
+        if (items.indexOf(item) !== items.length - 1) {
+            const hr = document.createElement('hr');
+            itemList.appendChild(hr);
+        }
+    });
+}
